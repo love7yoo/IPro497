3feeac5ecd7b444b369029290f5f0f01406eeb28
Giljae Yoo <love7yoo@gmail.com>
2021-03-03 11:46:13 +0900

    update database

--- models.py
+++ models.py
@@ -6,10 +6,37 @@ import base64
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(255))
+    __tablename__ = 'user'
+    email_address = db.Column(db.String(50), primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    building = db.Column(db.String(30), nullable=False)
+    room = db.Column(db.String(10), nullable=False)
+    reservation_status = db.Column(db.Boolean())
+    reservation_length = db.Column(db.Integer())
+
+class Reservation(db.Model):
+    __tablename__ = 'reservation'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email_address = db.Column(db.String(50), db.ForeignKey('user.email_address'))
+    email_address = db.relationship('User', foreign_keys=[email_address])
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    location_id = db.relationship('Location', foreign_keys=[id])
+    reservation_start = db.Column(db.DATETIME())
+    reservation_end = db.Column(db.DATETIME())
+
+class Open_hours(db.Model):
+    __tablename__ = 'open_hours'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Day = db.Column(db.INTEGER())
+    start_time = db.Column(db.DATETIME())
+    end_time = db.Column(db.DATETIME())
