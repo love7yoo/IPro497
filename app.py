78ea4d147d7ec81aeb6694fdebceea90860051b7
wmalak <wmalak@hawk.iit.edu>
2021-03-13 19:19:38 -0600

    Progress made on home page and new folder for images that will be used in site

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ db.init_app(app)
 def index():
     return render_template('index.html')
 
+#@app.route('/')
+#def user():
+#    return render_template('user/user.html')
+
+#@app.route('/')
+#def login():
+#    return render_template('login/login.html')
+
+#@app.route('/')
+#def sup(): #sign up page
+#    return render_template('sup/sup.html')
+
+#@app.route('/')
+#def location():
+#    return render_template('location/location.html')
+#@app.route('/')
+#def rnp(): #rules and policies
+#    return render_template('rnp/rnp.html')
+
 @app.route('/reservation')
 def reservation():
     return render_template('reservation/reservation.html')
